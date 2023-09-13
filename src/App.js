3997bc513fcd2c640bab4dfa82b4fd425f6ade31
jadsamara <81051893+jadsamara@users.noreply.github.com>
2023-09-13 01:47:04 -0400

    first test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const equation = 1 + 10;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-screen flex justify-center items-center">
+      <h1 className="text-3xl">Counter: {equation} </h1>
     </div>
   );
 }
